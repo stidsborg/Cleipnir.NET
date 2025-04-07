7c918ed2c1e2ea60a0571968ec35bddc5292f587
stidsborg <stidsborg@cleipnir.net>
2025-04-07 21:25:26 +0200

    TransientFailureFlow example

---
+++ Samples/Cleipnir.Flows.Samples.Console/TransientFailureHandling/TransientFailureFlow.cs
@@ -0,0 +1,25 @@
+using System.Net;
+
+namespace Cleipnir.Flows.Sample.ConsoleApp.TransientFailureHandling;
+
+public class TransientFailureFlow : Flow<string>
+{
+    private static readonly HttpClient HttpClient = new();
+    
+    public override async Task Run(string orderId)
+    {
+        for (var i = 1; i < 5; i++)
+        {
+            var success = await Capture(async () =>
+            {
+                var response = await HttpClient.PostAsync(
+                    "http://orderservice/api/orders/",
+                    new StringContent(orderId)
+                );
+                return response.StatusCode == HttpStatusCode.ServiceUnavailable;
+            });
+            if (!success)
+                await Delay(TimeSpan.FromSeconds(i ^ 2));
+        }
+    }
+}
